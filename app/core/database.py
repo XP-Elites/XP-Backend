477ad179bd242f8bf8f120836655f6d4e2a466ab
Romvulus Casunuran <mcrafterx@gmail.com>
2026-02-01 21:51:39 +0000

    Core now retrieves credentials from env, then ssm, then defaults to 'safe-ish' defaults.

--- app/core/database.py
+++ app/core/database.py
@@ -1,26 +1,47 @@
+import boto3
 import dotenv
+from botocore.exceptions import ClientError
 from sqlalchemy import create_engine, NullPool
+from mypy_boto3_ssm import SSMClient
 from sqlalchemy.orm import sessionmaker
 import os
 
 dotenv.load_dotenv()
 
 
-def _get_env_var(name: str) -> str:
-    """Retrieve a required environment variable or raise a clear error if missing."""
+def _get_var(name: str, default: str | None = None) -> str:
+    """Retrieve a required variable from SSM or environment or raise an error if missing."""
+
     value = os.getenv(name)
-    if value is None:
-        raise RuntimeError(f"Required environment variable '{name}' is not set.")
-    return value
+    if value is not None:
+        return value
+    try:
+        ssm_value = (ssm.get_parameter(Name=name, WithDecryption=True)
+                     .get("Parameter")
+                     .get("Value"))
+        if ssm_value is not None:
+            return ssm_value
+    except ssm.exceptions.ParameterNotFound:
+        pass
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "AccessDeniedException":
+            pass
+        else:
+            raise
+    if default is not None:
+        return default
+    raise RuntimeError(f"Required environment variable '{name}' is not set.")
+
+ssm: SSMClient = boto3.client("ssm")
 
 
 DATABASE_URL = (
     f"postgresql+psycopg://"
-    f"{_get_env_var('POSTGRES_USER')}:"
-    f"{_get_env_var('POSTGRES_PASSWORD')}@"
-    f"{_get_env_var('POSTGRES_HOST')}:"
-    f"{_get_env_var('POSTGRES_PORT')}/"
-    f"{_get_env_var('POSTGRES_DB')}"
+    f"{_get_var('XP_WEBSERVER_POSTGRES_USER', 'postgres')}:"
+    f"{_get_var('XP_WEBSERVER_POSTGRES_PASSWORD','postgres')}@"
+    f"{_get_var('XP_WEBSERVER_POSTGRES_HOST', 'localhost')}:"
+    f"{_get_var('XP_WEBSERVER_POSTGRES_PORT','5432')}/"
+    f"{_get_var('XP_WEBSERVER_POSTGRES_DB','api')}"
 )
 
 engine = create_engine(
